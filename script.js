0a8e03b32871907406f9ed2e8916dc83de13dfa2
Dayane Moraes <dayane.moraes2@gmail.com>
2022-11-28 19:49:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function calcularValorHora(){
+  
+  var nome = document.getElementById('input-nome').value;
+  var salarioMes = Number(document.querySelector('.input-salarioMes').value);
+  var diasSemanas = Number(document.getElementsByName('input-dia-semana')[0].value);
+  var horasDia = Number(document.getElementsByClassName('questionario')[3].value);
+
+  var nSemanas = 4;
+  var gastoComLanche = 150;
+
+  var diasMes = nSemanas * diasSemanas;
+  var horasTrabalhadasMes = diasMes * horasDia;
+
+  var salarioHora = salarioMes / horasTrabalhadasMes;
+  var horasTrabalhadasParaGastos = gastoComLanche / salarioHora;
+
+  document.querySelector('#nome').innerHTML = nome
+  document.getElementById('valor').innerHTML = salarioHora.toFixed(2)
+  document.getElementsByTagName('span')[2].innerHTML = horasTrabalhadasParaGastos.toFixed(0);
+  document.getElementById('chunda').classList.remove('invisivel')
+
+}
+
